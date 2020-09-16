4870d7f721f85753dc57c9e8d2e77de044be785a
naowal <naowalzaza@gmail.com>
2020-09-16 10:39:39 +0700

    Edit truck_control.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('generate/pdf/', views.generate_pdf, name='generate_pdf'),
+    path('generate/truck_control/', views.truck_control_pdf, name='truck_control_pdf'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ import tempfile
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/truck_control.html', {})
 
 def generate_pdf(request):
     """Generate pdf."""
@@ -23,6 +23,28 @@ def generate_pdf(request):
     # Creating http response
     response = HttpResponse(content_type='application/pdf;')
     response['Content-Disposition'] = 'inline; filename=list_entry.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        output = open(output.name, 'rb')
+        response.write(output.read())
+
+    return response
+
+def truck_control_pdf(request):
+    """Generate pdf."""
+    # Model data
+    #entryreport = Entry.objects.all().order_by('rating')
+
+    # Rendered
+    html_string = render_to_string('blog/truck_control.html')
+    html = HTML(string=html_string)
+    result = html.write_pdf()
+
+    # Creating http response
+    response = HttpResponse(content_type='application/pdf;')
+    response['Content-Disposition'] = 'inline; filename=truck_control.pdf'
     response['Content-Transfer-Encoding'] = 'binary'
     with tempfile.NamedTemporaryFile(delete=True) as output:
         output.write(result)
